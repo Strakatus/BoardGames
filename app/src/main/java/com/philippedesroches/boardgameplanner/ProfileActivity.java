cab4bcde3bc699d64fb57a1601c97de974aabb69
Strakatus <phdesro@videotron.ca>
2015-05-23 16:34:20 -0400

    Main menu created

--- app/src/main/java/com/philippedesroches/boardgameplanner/ProfileActivity.java
+++ app/src/main/java/com/philippedesroches/boardgameplanner/ProfileActivity.java
@@ -1,48 +1,23 @@
 package com.philippedesroches.boardgameplanner;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ProfileActivity extends ActionBarActivity {
 
-    private Button eventButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        eventButton = (Button)findViewById(R.id.eventButton);
-        eventButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onEventButtonClick();
-            }
-        });
-    }
-
-    private void onEventButtonClick(){
-        Intent myIntent = new Intent(MainActivity.this, EventActivity.class);
-        MainActivity.this.startActivity(myIntent);
-
-    }
-
-    private void onProfileButtonClick(){
-        Intent myIntent = new Intent(MainActivity.this, ProfileActivity.class);
-        MainActivity.this.startActivity(myIntent);
-
+        setContentView(R.layout.activity_profile);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
         return true;
     }
 
